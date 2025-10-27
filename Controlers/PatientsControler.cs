a2ac48a252171ce52e488978ab73857ee711f00b
Jędrzej <Jdrzej@DESKTOP-UV4BOJD>
2025-10-27 22:24:47 +0100

    add PatientsController

--- Controlers/PatientsControler.cs
+++ Controlers/PatientsControler.cs
@@ -1,6 +1,32 @@
-﻿namespace PatientManager.Api.Controlers
+﻿using Microsoft.AspNetCore.Mvc;
+using PatientManager.Api.Entities;
+
+namespace PatientManager.Api.Controlers
 {
-    public class PatientsControler
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PatientsControler : ControllerBase
     {
+        private readonly AppDbContext _context;
+
+        public PatientsControler(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var patients = _context.Patients.ToList();
+            return Ok(patients);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Patient patient)
+        {
+            _context.Patients.Add(patient);
+            _context.SaveChanges();
+            return Ok(patient);
+        }
     }
 }
