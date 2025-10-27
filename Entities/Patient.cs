7baf51cdd5d4a7103cc861d0195e8ce0af273438
Jędrzej <Jdrzej@DESKTOP-UV4BOJD>
2025-10-27 23:11:39 +0100

    Add patientPhoneNumber, migration and fix endpoint /patients

--- Entities/Patient.cs
+++ Entities/Patient.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string FirsName { get; set; }
         public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
         public string Pesel {  get; set; }
         public DateTime DateOfBirth { get; set; }
     }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using PatientManager.Api.Entities;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -12,10 +16,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 
 app.Run();
